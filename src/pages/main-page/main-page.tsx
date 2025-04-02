521d676e6eaa2c191d6e78f7cd3f35f8a3f91056
Your Name <you@example.com>
2025-04-02 20:50:51 +0700

    второй шаг, вытащил сортировку

--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -136,6 +136,41 @@ function MainPage ():JSX.Element {
                   </div>
                 </div>
                 <div className="catalog__content">
+                  <div className="catalog-sort">
+                    <form action="#">
+                      <div className="catalog-sort__inner">
+                        <p className="title title--h5">Сортировать:</p>
+                        <div className="catalog-sort__type">
+                          <div className="catalog-sort__btn-text">
+                            <input type="radio" id="sortPrice" name="sort" checked/>
+                            <label htmlFor="sortPrice">по цене</label>
+                          </div>
+                          <div className="catalog-sort__btn-text">
+                            <input type="radio" id="sortPopular" name="sort"/>
+                            <label htmlFor="sortPopular">по популярности</label>
+                          </div>
+                        </div>
+                        <div className="catalog-sort__order">
+                          <div className="catalog-sort__btn catalog-sort__btn--up">
+                            <input type="radio" id="up" name="sort-icon" checked aria-label="По возрастанию"/>
+                            <label htmlFor="up">
+                              <svg width="16" height="14" aria-hidden="true">
+                                <use xlinkHref="#icon-sort"></use>
+                              </svg>
+                            </label>
+                          </div>
+                          <div className="catalog-sort__btn catalog-sort__btn--down">
+                            <input type="radio" id="down" name="sort-icon" aria-label="По убыванию"/>
+                            <label htmlFor="down">
+                              <svg width="16" height="14" aria-hidden="true">
+                                <use xlinkHref="#icon-sort"></use>
+                              </svg>
+                            </label>
+                          </div>
+                        </div>
+                      </div>
+                    </form>
+                  </div>
                   <div className="cards catalog__cards">
                     {camerasCatalog.map((camera) => <ProductCard camera={camera} key={camera.id} onAddCameraInBasketClickButton={handleAddCameraToBasketButtonClick} />)}
                   </div>
