6fa5c3c180890b57944a30bbf8b5b74ff649fc23
Your Name <you@example.com>
2025-04-08 12:02:22 +0700

    добавил состояния,чтобы на них упираться в дальнейшем

--- src/components/search-form/search-form.tsx
+++ src/components/search-form/search-form.tsx
@@ -1,4 +1,15 @@
+import { useState } from 'react';
 function SearchForm () : JSX.Element {
+  const [query, setQuery] = useState('');
+  const [isListOpen, setIsListOpen] = useState(false);
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    setQuery(value);
+    setIsListOpen(value.length >= 3);
+  };
+
+
   return (
     <div className="form-search list-opened">
       <form>
@@ -6,7 +17,14 @@ function SearchForm () : JSX.Element {
           <svg className="form-search__icon" width="16" height="16" aria-hidden="true">
             <use xlinkHref="#icon-lens"></use>
           </svg>
-          <input className="form-search__input" type="text" autoComplete="off" placeholder="Поиск по сайту"/>
+          <input
+            className="form-search__input"
+            type="text"
+            autoComplete="off"
+            placeholder="Поиск по сайту"
+            value={query}
+            onChange={handleInputChange}
+          />
         </label>
         {/* <ul className="form-search__select-list scroller">
               <li className="form-search__select-item" tabIndex={0}>Cannonball Pro MX 8i</li>
