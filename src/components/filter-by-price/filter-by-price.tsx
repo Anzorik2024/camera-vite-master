94bc6c9b936d12e1625f52594b440e062c468d74
Your Name <you@example.com>
2025-04-18 17:14:58 +0700

    изменить условия, не работает

--- src/components/filter-by-price/filter-by-price.tsx
+++ src/components/filter-by-price/filter-by-price.tsx
@@ -1,6 +1,10 @@
+import { useState, ChangeEvent, KeyboardEvent, SyntheticEvent } from 'react';
 import { useAppSelector } from '../../hooks/use-app-selector';
 import { getCamerasMaxPrice,getCamerasMinPrice } from '../../store/selectors';
 import { UserInput } from '../../types/filter';
+import { QueryKey } from '../../const/query-key';
+import { useAppDispatch } from '../../hooks/use-app-dispatch';
+import { setMinPrice,setMaxPrice } from '../../store/filter-slice/filter-slice';
 
 type FilterByPriceProps = {
   bottomPrice: UserInput;
@@ -10,29 +14,152 @@ type FilterByPriceProps = {
 }
 function FilterByPrice({bottomPrice, topPrice, onBottomPriceChange, onTopPriceChange}: FilterByPriceProps): JSX.Element {
 
+  const dispatch = useAppDispatch();
+
   const minPrice = useAppSelector(getCamerasMinPrice);
   const maxPrice = useAppSelector(getCamerasMaxPrice);
 
+  const numBottomPrice = Number(bottomPrice);
+  const numTopPrice = Number(topPrice);
+
+  console.log('boottom',numBottomPrice);
+  console.log('top',numTopPrice);
+
+  const [isBottomPriceInvalid, setBottomPriceInvalid] = useState<boolean>(false);
+  const [isTopPriceInvalid, setTopPriceInvalid] = useState<boolean>(false);
+
+  const getValidBottomPrice = () => {
+    if (numBottomPrice !== 0) {
+      if (numBottomPrice < minPrice) {
+        return minPrice;
+      }
+      if (numBottomPrice > maxPrice && numTopPrice === 0) {
+        return maxPrice;
+      }
+
+      return bottomPrice;
+    }
+  };
+
+  const getValidTopPrice = () => {
+    if (numTopPrice > maxPrice && numTopPrice !== 0) {
+      return maxPrice;
+    }
+    if (numTopPrice < minPrice && numBottomPrice === 0) {
+      return minPrice;
+    }
+    return numTopPrice;
+  };
+
+  const handlePriceInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const price = event.target.value as UserInput;
+    const filterType = event.target.dataset.query as QueryKey;
+    const isNotValid = Number(price) <= 0;
+
+    switch(filterType) {
+      case QueryKey.BottomPrice : {
+        if (isNotValid) {
+          setBottomPriceInvalid(true);
+          onBottomPriceChange('');
+
+          return;
+        }
+
+        setBottomPriceInvalid(false);
+        onBottomPriceChange(price);
+        break;
+      }
+
+      case QueryKey.TopPrice :
+        if(isNotValid) {
+          setTopPriceInvalid(true);
+          onTopPriceChange('');
+
+          return;
+        }
+
+        setTopPriceInvalid(false);
+        onTopPriceChange(price);
+        break;
+    }
+  };
+
+  const handlePriceInputBlur = (event: SyntheticEvent) => {
+    const currentInput = event.target as HTMLInputElement;
+    const filterType = currentInput.dataset.query as QueryKey;
+
+    switch(filterType) {
+      case QueryKey.BottomPrice: {
+        const validBottomPrice = getValidBottomPrice();
+
+        if (validBottomPrice) {
+          onBottomPriceChange(validBottomPrice);
+          dispatch(setMinPrice(validBottomPrice));//устанавливаем значение в плейсхолдер
+
+
+          // добавить диспатч
+        }
+
+        break;
+      }
+
+      case QueryKey.TopPrice: {
+        const validTopPrice = getValidTopPrice();
+
+        if (validTopPrice) {
+          onTopPriceChange(validTopPrice);
+          //добавить диспатч
+          dispatch(setMaxPrice(validTopPrice));
+        }
+
+        break;
+      }
+    }
+
+    if (numBottomPrice === 0) {
+      onBottomPriceChange('');
+      setBottomPriceInvalid(false);
+    }
+    if (numTopPrice === 0) {
+      onTopPriceChange('');
+      setTopPriceInvalid(false);
+    }
+  };
+
+  const handleInputKeyDown = (event: KeyboardEvent) => {
+    const input = event.target as HTMLInputElement;
+    if (event.key === 'Enter') {
+      input.blur();
+    }
+  };
 
   return (
     <fieldset className="catalog-filter__block">
       <legend className="title title--h5">Цена, ₽</legend>
       <div className="catalog-filter__price-range">
-        <div className="custom-input">
+        <div className={`custom-input ${isBottomPriceInvalid ? 'is-invalid' : '' }`}>
           <label>
             <input
               type="number"
               name="price"
               placeholder={`${minPrice}`}
+              onChange={handlePriceInputChange}
+              data-query={QueryKey.BottomPrice}
+              onBlur={handlePriceInputBlur}
+              onKeyDown={handleInputKeyDown}
             />
           </label>
         </div>
-        <div className="custom-input">
+        <div className={`custom-input ${isTopPriceInvalid ? 'is-invalid' : '' }`}>
           <label>
             <input
               type="number"
               name="priceUp"
               placeholder={`${maxPrice}`}
+              onChange={handlePriceInputChange}
+              data-query={QueryKey.TopPrice}
+              onBlur={handlePriceInputBlur}
+              onKeyDown={handleInputKeyDown}
             />
           </label>
         </div>
---
+++ src/const/query-key.ts
@@ -0,0 +1,12 @@
+export enum QueryKey {
+  Search = 'name_like',
+  SortOrder = '_order',
+  SortType = '_sort',
+  FilterType = 'type',
+  FilterCategory = 'category',
+  FilterLevel = 'level',
+  BottomPrice = 'price_gte',
+  TopPrice = 'price_lte',
+  Limit = '_limit',
+  Embed = '_embed'
+}
