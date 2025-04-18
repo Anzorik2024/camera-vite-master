ae90d9606ef6dd5411fe30060ea9e7782e56453e
Your Name <you@example.com>
2025-04-18 11:44:36 +0700

    создан фильтры, перенесен цена

---
+++ src/components/filters/filters.tsx
@@ -0,0 +1,72 @@
+import FilterByPrice from '../filter-by-price/filter-by-price';
+function Filters(): JSX.Element {
+
+  return (
+    <div className="catalog__aside">
+      <div className="catalog-filter">
+        <form action="#">
+          <h2 className="visually-hidden">Фильтр</h2>
+          <FilterByPrice/>
+          <fieldset className="catalog-filter__block">
+            <legend className="title title--h5">Категория</legend>
+            <div className="custom-radio catalog-filter__item">
+              <label>
+                <input type="radio" name="category" value="photocamera" checked/><span className="custom-radio__icon"></span><span className="custom-radio__label">Фотокамера</span>
+              </label>
+            </div>
+            <div className="custom-radio catalog-filter__item">
+              <label>
+                <input type="radio" name="category" value="videocamera"/><span className="custom-radio__icon"></span><span className="custom-radio__label">Видеокамера</span>
+              </label>
+            </div>
+          </fieldset>
+          <fieldset className="catalog-filter__block">
+            <legend className="title title--h5">Тип камеры</legend>
+            <div className="custom-checkbox catalog-filter__item">
+              <label>
+                <input type="checkbox" name="digital" checked/><span className="custom-checkbox__icon"></span><span className="custom-checkbox__label">Цифровая</span>
+              </label>
+            </div>
+            <div className="custom-checkbox catalog-filter__item">
+              <label>
+                <input type="checkbox" name="film" disabled/><span className="custom-checkbox__icon"></span><span className="custom-checkbox__label">Плёночная</span>
+              </label>
+            </div>
+            <div className="custom-checkbox catalog-filter__item">
+              <label>
+                <input type="checkbox" name="snapshot"/><span className="custom-checkbox__icon"></span><span className="custom-checkbox__label">Моментальная</span>
+              </label>
+            </div>
+            <div className="custom-checkbox catalog-filter__item">
+              <label>
+                <input type="checkbox" name="collection" checked disabled/><span className="custom-checkbox__icon"></span><span className="custom-checkbox__label">Коллекционная</span>
+              </label>
+            </div>
+          </fieldset>
+          <fieldset className="catalog-filter__block">
+            <legend className="title title--h5">Уровень</legend>
+            <div className="custom-checkbox catalog-filter__item">
+              <label>
+                <input type="checkbox" name="zero" checked/><span className="custom-checkbox__icon"></span><span className="custom-checkbox__label">Нулевой</span>
+              </label>
+            </div>
+            <div className="custom-checkbox catalog-filter__item">
+              <label>
+                <input type="checkbox" name="non-professional"/><span className="custom-checkbox__icon"></span><span className="custom-checkbox__label">Любительский</span>
+              </label>
+            </div>
+            <div className="custom-checkbox catalog-filter__item">
+              <label>
+                <input type="checkbox" name="professional"/><span className="custom-checkbox__icon"></span><span className="custom-checkbox__label">Профессиональный</span>
+              </label>
+            </div>
+          </fieldset>
+          <button className="btn catalog-filter__reset-btn" type="reset">Сбросить фильтры
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Filters;
--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -18,7 +18,7 @@ import { WarningMessage } from '../../const/warning-message';
 import Sort from '../../components/sort/sort';
 import { getCurrentSortOrder, getCurrentSortType} from '../../store/selectors';
 import { sortCameras } from '../../utils/sort-cameras';
-import FilterByPrice from '../../components/filter-by-price/filter-by-price'; // вынести в фильтр компонент
+import Filters from '../../components/filters/filters';
 function MainPage ():JSX.Element {
 
   const [isModalAddCameraToBasketOpen, setModalAddCameraToBasketOpen] = useState<boolean>(false);
@@ -71,70 +71,7 @@ function MainPage ():JSX.Element {
             <div className="container">
               <h1 className="title title--h2">Каталог фото- и видеотехники</h1>
               <div className="page-content__columns">
-                <div className="catalog__aside">
-                  <div className="catalog-filter">
-                    <form action="#">
-                      <h2 className="visually-hidden">Фильтр</h2>
-                      <FilterByPrice/>
-                      <fieldset className="catalog-filter__block">
-                        <legend className="title title--h5">Категория</legend>
-                        <div className="custom-radio catalog-filter__item">
-                          <label>
-                            <input type="radio" name="category" value="photocamera" checked/><span className="custom-radio__icon"></span><span className="custom-radio__label">Фотокамера</span>
-                          </label>
-                        </div>
-                        <div className="custom-radio catalog-filter__item">
-                          <label>
-                            <input type="radio" name="category" value="videocamera"/><span className="custom-radio__icon"></span><span className="custom-radio__label">Видеокамера</span>
-                          </label>
-                        </div>
-                      </fieldset>
-                      <fieldset className="catalog-filter__block">
-                        <legend className="title title--h5">Тип камеры</legend>
-                        <div className="custom-checkbox catalog-filter__item">
-                          <label>
-                            <input type="checkbox" name="digital" checked/><span className="custom-checkbox__icon"></span><span className="custom-checkbox__label">Цифровая</span>
-                          </label>
-                        </div>
-                        <div className="custom-checkbox catalog-filter__item">
-                          <label>
-                            <input type="checkbox" name="film" disabled/><span className="custom-checkbox__icon"></span><span className="custom-checkbox__label">Плёночная</span>
-                          </label>
-                        </div>
-                        <div className="custom-checkbox catalog-filter__item">
-                          <label>
-                            <input type="checkbox" name="snapshot"/><span className="custom-checkbox__icon"></span><span className="custom-checkbox__label">Моментальная</span>
-                          </label>
-                        </div>
-                        <div className="custom-checkbox catalog-filter__item">
-                          <label>
-                            <input type="checkbox" name="collection" checked disabled/><span className="custom-checkbox__icon"></span><span className="custom-checkbox__label">Коллекционная</span>
-                          </label>
-                        </div>
-                      </fieldset>
-                      <fieldset className="catalog-filter__block">
-                        <legend className="title title--h5">Уровень</legend>
-                        <div className="custom-checkbox catalog-filter__item">
-                          <label>
-                            <input type="checkbox" name="zero" checked/><span className="custom-checkbox__icon"></span><span className="custom-checkbox__label">Нулевой</span>
-                          </label>
-                        </div>
-                        <div className="custom-checkbox catalog-filter__item">
-                          <label>
-                            <input type="checkbox" name="non-professional"/><span className="custom-checkbox__icon"></span><span className="custom-checkbox__label">Любительский</span>
-                          </label>
-                        </div>
-                        <div className="custom-checkbox catalog-filter__item">
-                          <label>
-                            <input type="checkbox" name="professional"/><span className="custom-checkbox__icon"></span><span className="custom-checkbox__label">Профессиональный</span>
-                          </label>
-                        </div>
-                      </fieldset>
-                      <button className="btn catalog-filter__reset-btn" type="reset">Сбросить фильтры
-                      </button>
-                    </form>
-                  </div>
-                </div>
+                <Filters/>
                 <div className="catalog__content">
                   <Sort/>
                   <div className="cards catalog__cards">
