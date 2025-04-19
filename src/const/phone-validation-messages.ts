4a804794a5fb5ec97ea3b0a1730541a5b0c5f54c
Your Name <you@example.com>
2025-04-19 20:32:51 +0700

    исправление ошибок

--- src/const/phone-validation-messages.ts
+++ src/const/phone-validation-messages.ts
@@ -1,8 +1,8 @@
 enum PhoneValidationMessages {
   REQUIRED = 'Нужно указать номер',
-  LENGTH = 'Номер должен содержать 10 цифр после +7',
+  LENGTH = 'Номер содержит 10 цифр после +7',
   INVALID_CHARACTERS = 'Недопустимо. Введите только цифры.',
-  EXCESSIVE_LENGTH = 'Номер — 12 символов. Сотрите лишние символы'
+  EXCESSIVE_LENGTH = 'Сотрите лишние символы'
 }
 
 export default PhoneValidationMessages;
