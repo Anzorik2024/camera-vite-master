ae2b996ddbf6e2e0ce898293f166053eda8deb8d
Your Name <you@example.com>
2025-04-25 22:03:44 +0700

    чистка констант

--- src/const/query-key.ts
+++ src/const/query-key.ts
@@ -1,5 +1,4 @@
 export enum QueryKey {
-  Search = 'name_like',
   SortOrder = '_order',
   SortType = '_sort',
   FilterType = 'type',
@@ -7,6 +6,4 @@ export enum QueryKey {
   FilterLevel = 'level',
   BottomPrice = 'price_gte',
   TopPrice = 'price_lte',
-  Limit = '_limit',
-  Embed = '_embed'
 }
